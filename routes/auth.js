14d992d93f901f1f9b51eeae035f12d76976e574
Buğra BASLI <43937051+bbasli@users.noreply.github.com>
2021-04-07 15:09:39 +0300

    User validation added

--- routes/auth.js
+++ routes/auth.js
@@ -10,13 +10,30 @@ const router = express.Router();
 router.put(
   "/signup",
   [
-    body("email").custom((value) => {
-      return User.findOne({ email: value }).then((user) => {
-        if (user) {
-          return Promise.reject("The user already exists!");
-        }
-      });
-    }),
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) return Promise.reject("Email is already used!");
+        });
+      })
+      .normalizeEmail(),
+    body("password")
+      .trim()
+      .isLength({ min: 5, max: 30 })
+      .withMessage("Password must be at least 5 characters")
+      .matches(/\d/)
+      .withMessage("Password must contain a number"),
+    body("username")
+      .trim()
+      .isLength({ min: 5, max: 30 })
+      .withMessage("Username must be at least 5 characters")
+      .custom((value) => {
+        return User.findOne({ username: value }).then((user) => {
+          if (user) return Promise.reject("Username is already used!");
+        });
+      }),
   ],
   authController.signup
 );
--- routes/contact.js
+++ routes/contact.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const { body } = require("express-validator");
 
-const Contact = require("../models/user");
+const User = require("../models/user");
 const contactController = require("../controllers/contact");
 
 const router = express.Router();
 
-router.put("/", contactController.addContact);
+router.put(
+  "/",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("message")
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage("Message must be at least 5 characters"),
+  ],
+  contactController.addContact
+);
 
 module.exports = router;
